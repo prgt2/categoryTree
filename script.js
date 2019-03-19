19fda3c9c607f0b35e2d7b516191e72e03c0bc8f
inawe <eindeloosverval@gmail.com>
2019-03-19 19:59:28 +0100

    tried to get data from url, parse it into JS and write in document; failed

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+$(document).ready(function() {
+    $("button").click(function) {
+        $get("https://www.lionsbet.com/rest/market/categories", function(data) {
+            document.querySelector('.place').innerHTML = JSON.parse(data);
+            console.log(data);
+        }, JSON);
+    });
+});
+
+
