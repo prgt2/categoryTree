65f68866a083b18fde7d66c5e8726221444b650c
inawe <eindeloosverval@gmail.com>
2019-03-19 21:23:54 +0100

    2123

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
+function skrypt() {
+    //$(document).ready(function() {
+        $("button").click(function) {
+            $get("https://www.lionsbet.com/rest/market/categories", function(data) {
+                document.querySelector('.place').innerHTML = data;
+                console.log(data);
+            }, JSON);
+        };
+    //});
 
-$(document).ready(function() {
-    $("button").click(function) {
-        $get("https://www.lionsbet.com/rest/market/categories", function(data) {
-            document.querySelector('.place').innerHTML = JSON.parse(data);
-            console.log(data);
-        }, JSON);
-    });
-});
-
-
+}
