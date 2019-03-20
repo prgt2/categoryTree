8099a7b0ac81f8b786a5e342e098db3e41fc350d
randomqueen <martakubis@mail.ru>
2019-03-20 01:11:22 +0100

    . [object Object],[object Object]  przez append

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function skrypt() {
-    //$(document).ready(function() {
-        //$("button").click(function) {
-            $.getJSON('https://www.lionsbet.com/rest/market/categories', function(data) {
-                //document.querySelector('.place').innerHTML = data;
-                console.log(data);
-                var text = "hshs" ;//+ ${data[5]};
-                $("place").html(data);
+   // $(document).ready(function() {
+     //   $("button").click(function() {
+            $.getJSON('https://www.lionsbet.com/rest/market/categories', data1, function(data1){
+                document.querySelector('.place').innerHTML = data1;
+                console.log(data1);
             });
-            
-       // };
-    //});
+            var text = "hshs" ;//+ ${data[5]};
+    //  })   
+    //})
+    };
 
-}
+function skrypt2(){
+    $(document).ready(function(){
+        $("button").click(function(){
+          $.getJSON("https://www.lionsbet.com/rest/market/categories", function(result){
+            $.each(result, function(i, field){
+              $("#div01").append(field + " ");
+            });
+          });
+        });
+      });
+
+};
