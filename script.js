89e802927ce412853c814f499d1ab84f834d3728
Beniamin Gorgon <beniamin.gorgon@pmchydraulics.com>
2019-03-20 11:43:28 +0100

    resolved JSON processing

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function skrypt() {
     //$(document).ready(function() {
         //$("button").click(function) {
             $.getJSON('https://www.lionsbet.com/rest/market/categories', function(data) {
-                //document.querySelector('.place').innerHTML = data;
-                console.log(data);
-                var text = "hshs" ;//+ ${data[5]};
+                //var a = JSON.parse(data.data)
+                document.querySelector('.place').innerHTML = data.data[5].categoryName;
+                console.log(a);
+                var text = data ;//+ ${data[5]};
                 $("place").html(data);
             });
             
