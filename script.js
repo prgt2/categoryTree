54ffe9ffa03e540fd67ec99777214a4e375254c9
purgator2000 <46408466+purgator2000@users.noreply.github.com>
2019-03-21 20:38:19 +0100

    Comments on komentarzobject

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function skrypt() {
                 console.log(a);
                 var text = data ;//+ ${data[5]};
                 $("place").html(data);
->>>>>>> 89e802927ce412853c814f499d1ab84f834d3728
+>>>>>>> 89e802927ce412853c814f499d1ab84f834d3728 //TU KONFLIKT, ALE NIE WIEM CZEMU POWSTAŁ - MOŻE PUSZOWAŁAŚ Z NIEAKTUALNEGO PULLA
             });
             var text = "hshs" ;//+ ${data[5]};
     //  })   
@@ -25,10 +25,10 @@ function skrypt() {
 //function skrypt2(){
     $(document).ready(function(){
         $("#but02").click(function(){     //tu w HTML nie ma onclick, za to tu jest click(). Obie metody są wymienne, ale ta ponoć nowocześniejsza. Ogólnie wygląda, że skrypty komunikują się ze sobą jako równorzędne (brak centralnego sterowania?)   
-          $.getJSON("https://www.lionsbet.com/rest/market/categories", function(result){
+          $.getJSON("https://www.lionsbet.com/rest/market/categories", function(result){ // CIEKAWE CZEMU TU RESULT ZAMIAST DATA
             $.each(result, function(i, field){    //za https://www.w3schools.com/jquery/ajax_getjson.asp  i nie do końca kumam jak działa
               $("#id02").html(field + " ");  // tu html() podmienia tekst na obiekt,a append dopisuje obiekt do tekstu. load() tu nie działa.
-            });
+            }); //ZUPEŁNIE NIE ROZUMIEM OD EACH W DÓŁ, NA W3 JEST TYLKO KOD BEZ WYJAŚNIENIA
           });
         });
       });
