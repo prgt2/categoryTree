74af37e1307537707e4b49768ee62a8426f275c7
inawe <eindeloosverval@gmail.com>
2019-03-23 10:40:28 +0100

    sorted sportyLvl1Sorted

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 function skrypt() {
-    //$(document).ready(function() {
-        //$("button").click(function) {
-            $.getJSON('https://www.lionsbet.com/rest/market/categories', function(data) {
-                //var a = JSON.parse(data.data)
-                document.querySelector('.place').innerHTML = data.data[5].categoryName;
-                console.log(a);
-                var text = data ;//+ ${data[5]};
-                $("place").html(data);
-            });
-            
-       // };
-    //});
+    $.getJSON('https://www.lionsbet.com/rest/market/categories', function(data) {
+    document.querySelector('.place').innerHTML = data.data[5].categoryName;
+    var sporty = data.data;
+    var sportyLvl1 = [];
+    var sportyLvl1Sorted = [];
+    for (i = 0; i <sporty.length; i++) {
+        if (sporty[i].level === 1) {
+            sportyLvl1.push(sporty[i]);
+        }   
+    }
+    /*function compare(a, b) {
+        a.categoryId < b.categoryId ? -1 :  1;
+    }
+    sportyLvl1Sorted = sportyLvl1.sort(compare)
+    console.log(sportyLvl1);
+    console.log(sportyLvl1Sorted);  */
+    
+    sportyLvl1Sorted = sportyLvl1.sort(function(a, b) {
+            return a.categoryId - b.categoryId;
+        });
+        document.getElementById("demo").innerHTML = sportyLvl1Sorted[6].sportName;
+        console.log(sportyLvl1Sorted);
+    });
+};
 
-}
