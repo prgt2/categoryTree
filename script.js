3eb8008e32b46b9de28f3e74026c04e12d9a72b2
inawe <eindeloosverval@gmail.com>
2019-03-24 13:08:33 +0100

    messed JSON data

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
 function skrypt() {
-    $.getJSON('https://www.lionsbet.com/rest/market/categories', function(data) {
-    document.querySelector('.place').innerHTML = data.data[5].categoryName;
-    var sporty = data.data;
-    var sportyLvl1 = [];
-    var sportyLvl1Sorted = [];
-    for (i = 0; i <sporty.length; i++) {
-        if (sporty[i].level === 1) {
-            sportyLvl1.push(sporty[i]);
-        }   
+    var data = $.getJSON('https://www.lionsbet.com/rest/market/categories');
+    //document.querySelector('.place').innerHTML = data.data[5].categoryName;
+    function dodaj(a, d, e) { // wypluwa posortowaną tablicę obiektów o zadanym levelu d z JSONA a
+        var b = []; // b - tablica z obiektami o zadanym levelu
+        for (i = 0; i <a.length; i++) { // a - caly JSON
+            if (a[i].level === d && a[i].parentCategory === e) { // d - zadany level obiektu
+                b.push(a[i]); // b - tablica z obiektami o zadanym levelu
+            }   
+        }
+        c = b.sort(function(x, y) { // c - posortowane obiekty (e fotmie tablicy?)
+                return x.categoryId - y.categoryId;
+            });
+        return c;
     }
-    sportyLvl1Sorted = sportyLvl1.sort(function(a, b) {
-            return a.categoryId - b.categoryId;
-        });
-    //document.getElementById('demo').innerHTML = sportyLvl1Sorted[5].categoryName;
-    console.log(sportyLvl1Sorted[5].categoryName);
-    for (i = 0; i < sportyLvl1.length; i++) {
+    var sportyLvl1Sorted = dodaj(data, 1, 0);
+    console.log(sportyLvl1Sorted);
+    console.log(data[3]);
+    for (i = 0; i < sportyLvl1Sorted.length; i++) {
         nowyElement = document.createElement('li');
         nowyElement.innerHTML = sportyLvl1Sorted[i].categoryName;
         listaSportyLvl1 = document.getElementById('sportyLvl1');
         listaSportyLvl1.appendChild(nowyElement);
     }
-    //console.log(sportyLvl1Sorted[5].categoryName);
-    });
+    var MasterObject = new Object();
+    MasterObject.lvl1 = function() {
+        var lvl1 = dodaj(data.data, 1);
+        
+    }
 };
-
